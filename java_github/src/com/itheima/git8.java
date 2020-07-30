3b7a8a69e5e088ff11e1de76c7a258e4db9872ad
admin <admin>
2020-07-30 16:01:58 +0800

    把本地工程添加到git

---
+++ java_github/src/com/itheima/git8.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class git8 {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
